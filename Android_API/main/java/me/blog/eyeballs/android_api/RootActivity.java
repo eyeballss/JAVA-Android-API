cb58e7b322e05e681265a93d91b42d2836f95fbb
eyeballs <kkvv1111@gmail.com>
2017-07-31 07:31:38 -0700

    added isNetworkAvailable and Preference API

--- Android_API/main/java/me/blog/eyeballs/android_api/RootActivity.java
+++ Android_API/main/java/me/blog/eyeballs/android_api/RootActivity.java
@@ -45,4 +45,101 @@ public class RootActivity extends AppCompatActivity {
                 .show();
     }
 
+    //네트워크 연결 확인
+    protected int isNetworkConnected(){
+        int isConnected = 0;
+
+        ConnectivityManager manager =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mobile = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo wifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (mobile.isConnected()){
+            isConnected = 1;
+        }else if(wifi.isConnected()){
+            isConnected = 2;
+        }else{
+            isConnected = 0;
+        }
+        return isConnected;
+    }
+
+
+
+    //SharedPreferences 메소드들
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    protected  void savePreferences(String fileName, String key, String value){
+        initPreferences(fileName);
+        editor.putString(key, value);
+        editor.commit();
+    }
+    protected  void savePreferences(String fileName, String key, boolean value){
+        initPreferences(fileName);
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+    protected  void savePreferences(String fileName, String key, int value){
+        initPreferences(fileName);
+        editor.putInt(key, value);
+        editor.commit();
+    }
+    protected  void savePreferences(String fileName, String key, float value){
+        initPreferences(fileName);
+        editor.putFloat(key, value);
+        editor.commit();
+    }
+    protected  void savePreferences(String fileName, String key, long value){
+        initPreferences(fileName);
+        editor.putLong(key, value);
+        editor.commit();
+    }
+    protected  void savePreferences(String fileName, String key, Set<String> value){
+        initPreferences(fileName);
+        editor.putStringSet(key, value);
+        editor.commit();
+    }
+
+    protected String readStringPreferences(String fileName, String key){
+        preferences = getSharedPreferences(fileName, MODE_PRIVATE);
+        return preferences.getString(key, null);
+    }
+    protected int readIntPreferences(String fileName, String key){
+        preferences = getSharedPreferences(fileName, MODE_PRIVATE);
+        return preferences.getInt(key, 0);
+    }
+    protected boolean readBooleanPreferences(String fileName, String key){
+        preferences = getSharedPreferences(fileName, MODE_PRIVATE);
+        return preferences.getBoolean(key, false);
+    }
+    protected float readFloatPreferences(String fileName, String key){
+        preferences = getSharedPreferences(fileName, MODE_PRIVATE);
+        return preferences.getFloat(key, 0.0f);
+    }
+    protected long readLongPreferences(String fileName, String key){
+        preferences = getSharedPreferences(fileName, MODE_PRIVATE);
+        return preferences.getLong(key, 0);
+    }
+    public  Set<String> readStingSetPreferences(String fileName, String key){
+        preferences = getSharedPreferences(fileName, MODE_PRIVATE);
+        return preferences.getStringSet(key, null);
+    }
+
+    protected void removeAllPreferences(String fileName){
+        initPreferences(fileName);
+        editor.clear();
+        editor.commit();
+    }
+
+    protected void removePreferences(String fileName, String key){
+        initPreferences(fileName);
+        editor.remove(key);
+        editor.commit();
+    }
+
+    private void initPreferences(String fileName){
+        preferences = getSharedPreferences(fileName, MODE_PRIVATE);
+        editor = preferences.edit();
+    }
 }
--- Android_API/main/java/me/blog/eyeballs/android_api/TestActivity.java
+++ Android_API/main/java/me/blog/eyeballs/android_api/TestActivity.java
@@ -2,8 +2,15 @@ package me.blog.eyeballs.android_api;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TestActivity extends RootActivity {
+
+    private EditText TestAct_input;
+    private final String FILENAME = "FILENAME";
+    private final String KEY = "KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,8 +18,11 @@ public class TestActivity extends RootActivity {
         setContentView(R.layout.activity_test);
 
         Utility.Log("TestActivity","이런 식으로 로그를 씁니다.");
-        testDialog();
+        testPreference();
+//        testDialog();
     }
+
+
 
 
     //로딩을 2초간 보여준 후 에러 메세지를 띄운다.
@@ -25,5 +35,41 @@ public class TestActivity extends RootActivity {
             }
         }, 2000);
 
+    }
+
+    //네트워크가  연결되어 있다면
+    public void isNetworkAvailable(View view){
+        int status = isNetworkConnected();
+        String result=null;
+        switch(status){
+            case 0:
+                result="Network is connected!";
+                break;
+            case 1:
+                result="Network is connected with mobile data!";
+                break;
+            case 2:
+                result="Network is connected with wifi!";
+                break;
+        }
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void testPreference() {
+        TestAct_input = (EditText) findViewById(R.id.TestAct_input);
+        String result = readStringPreferences(FILENAME, KEY);
+        if(result != null){
+            TestAct_input.setText(result);
+            Toast.makeText(this, "불러왔습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void saveData(View view){
+        savePreferences(FILENAME, KEY, TestAct_input.getText().toString());
+        Toast.makeText(this, "저장했습니다.", Toast.LENGTH_SHORT).show();
+    }
+    public void deleteData(View view){
+        removeAllPreferences(FILENAME);
+        Toast.makeText(this, "저장된 데이터를 모두 지웠습니다.", Toast.LENGTH_SHORT).show();
     }
 }
